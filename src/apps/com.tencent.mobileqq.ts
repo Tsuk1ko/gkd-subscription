5f19081dfa21c19adbb7275fa14f41035b3d6a24
神代綺凛 <i@loli.best>
2026-09-09 01:51:54 +0800

    feat: QQ auto click favorite sticker tab

--- src/apps/com.tencent.mobileqq.ts
+++ src/apps/com.tencent.mobileqq.ts
@@ -18,5 +18,27 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 1,
+      name: '功能类-表情面板自动切换至收藏表情',
+      desc: '打开表情面板时自动点击收藏表情Tab',
+      fastQuery: true,
+      activityIds: 'com.tencent.mobileqq.activity.SplashActivity',
+      rules: [
+        {
+          key: 0,
+          name: '记录表情面板已收起',
+          action: 'none',
+          matches: '[vid="tab_sencondary_emoticon"][visibleToUser=false]',
+        },
+        {
+          key: 1,
+          name: '点击收藏表情Tab',
+          preKeys: [0],
+          matches:
+            '@LinearLayout[desc="收藏表情面板"][visibleToUser=true] <n [vid="tab_sencondary_emoticon"][visibleToUser=true]',
+        },
+      ],
+    },
   ],
 });
